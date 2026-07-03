3d663ff67d519b731b52129fd1e266a4d6e6b667
Aryan Sethi <sethiaryan2001@gmail.com>
2026-07-03 19:29:32 +0530

    Added flight controls alongside cinemachine follow camera
    Currently has an issue with vector3 calculations, causing front back left right to be assigned to the wrong joystick directions

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -13,22 +13,37 @@ public class FlightController : MonoBehaviour
     [SerializeField] public float yawMulitiplier;
     [SerializeField] public float pitchMultiplier;
 
-    new Rigidbody rb;
+    [SerializeField] private Vector3 moveDirection;
+
+    [SerializeField] new Rigidbody rb;
+
+    public InputActionReference fly;
+
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        moveDirection = fly.action.ReadValue<Vector3>();
+    }
+
     private void FixedUpdate()
     {
+
+        
+
         float deltaTime = Time.fixedDeltaTime;
         float pitch = Input.GetAxis("Vertical");
 
         float yaw = Input.GetAxis("Horizontal");
 
-        rb.AddRelativeForce(0f, (thrust * thrustMultiplier * deltaTime), 0f);
-        rb.AddRelativeForce((pitch * pitchMultiplier * deltaTime), (yaw * yawMulitiplier * deltaTime), (-yaw * yawMulitiplier * deltaTime));
+        //rb.AddRelativeForce(0f, (thrust * thrustMultiplier * deltaTime), 0f);
+        //rb.AddRelativeForce((pitch * pitchMultiplier * deltaTime), (yaw * yawMulitiplier * deltaTime), (-yaw * yawMulitiplier * deltaTime));
+        rb.linearVelocity = new Vector3((pitch * pitchMultiplier * deltaTime), (yaw * yawMulitiplier * deltaTime), (-yaw * yawMulitiplier * deltaTime));
     }
 
 }
